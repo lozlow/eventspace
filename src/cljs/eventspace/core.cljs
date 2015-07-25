9bacfa24e9f5db0a5c514708ea84e4a162ae1850
Pete Shaw <peeta.shaw@gmail.com>
2015-07-25 22:31:18 +0100

    Added a fake loading panel for the future

--- src/cljs/eventspace/core.cljs
+++ src/cljs/eventspace/core.cljs
@@ -1,6 +1,6 @@
 (ns eventspace.core
   (:require [reagent.core :as reagent :refer [atom]]
-            [re-frame.core :refer [dispatch-sync]]
+            [re-frame.core :refer [subscribe dispatch-sync]]
             [eventspace.menu.core :as menu]
             [eventspace.space.core :as space]
             [eventspace.database]
@@ -9,11 +9,19 @@
 
 (enable-console-print!)
 
+(defn render-main
+  []
+  (let [space (subscribe [:selected-space])]
+    (fn []
+      (if @space
+        [space/render-space]
+        [space/loading-panel]))))
+
 ;; -------------------------
 ;; Initialize app
 (defn mount-components []
-  (reagent/render-component menu/render-menu (.getElementById js/document "menu"))
-  (reagent/render-component [space/render-space] (.getElementById js/document "app")))
+  (reagent/render-component [menu/render-menu] (.getElementById js/document "menu"))
+  (reagent/render-component [render-main] (.getElementById js/document "app")))
 
 (defn ^:export init! []
   (dispatch-sync [:initialise-database])
